d3ada5a341076e218efb4883db5aca0032d76847
Simranjeet Singh <smrnjeet222@gmail.com>
2021-03-11 19:32:20 +0530

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import "firebase/auth"
+
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MEASSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+
+export const auth = app.auth();
+
+export default app;
